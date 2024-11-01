3f9d577b6e2f9440b3f65c0a2f8293b18dd7a8e5
Quyen Tran <156051620+quyenlhtran@users.noreply.github.com>
2024-11-01 10:49:49 -0400

    Update ContainerItem.java

--- CSC125_Project/ContainerItem.java
+++ CSC125_Project/ContainerItem.java
@@ -1,18 +1,50 @@
+/**
+ * The ContainerItem class represents a specialized item that can hold other items.
+ * It inherits from the Item class
+ */
+
 import java.util.ArrayList;
+
 public class ContainerItem extends Item
 {
+    // Member variables
+    /**
+     * items: the list of items contained within this container.
+     */
     private ArrayList<Item> items;
+
+    // Constructor
+    /**
+     * Constructs a new Item object from name, type, and description inherits from Item class
+     * @param pName the name of the item inherits from Item class
+     * @param pType the type of the item inherits from Item class
+     * @param pDescription the description of the item inherits from Item class
+     * Initializes an empty ArrayList to store items within the container.
+     */
     public ContainerItem(String pName, String pType, String pDescription){
         super(pName, pType, pDescription);
         items = new ArrayList<Item>();
     }
     
-    public void addItem(Item pItem){
+    //Methods
+    /**
+     * This method adds an item to the container
+     * @param pItem The item to be added to the container
+     */
+    public void addItem(Item pItem)
+    {
         items.add(pItem);
     }
 
-    public boolean hasItem(String pName){
-        for(int i = 0; i < items.size(); i++){
+    /**
+     * This method checks if an item with the specified name exists in the container
+     * @param pName The name of the item to check for
+     * @return true if the item exists in the container, false otherwise
+     */
+    public boolean hasItem(String pName)
+    {
+        for(int i = 0; i < items.size(); i++)
+        {
             if(items.get(i).getName().toLowerCase().equals(pName)){
                 return true;
             }
@@ -20,7 +52,14 @@ public class ContainerItem extends Item
         return false;
     }
 
-    public Item removeItem(String pName){
+    
+    /**
+     * This method removes and returns an item with the specified name from the container
+     * @param pName The name of the item to remove
+     * @return The removed item if found, null if no item with the specified name exists
+     */
+    public Item removeItem(String pName)
+    {
         Item removedItem;
         for(int i = 0; i < items.size(); i++){
             if(items.get(i).getName().toLowerCase().equals(pName)){
@@ -32,6 +71,12 @@ public class ContainerItem extends Item
         return null;
     }
 
+    /**
+     * This method returns a string representation of the container item, including its name, type,
+     * description, and details of any contained items. 
+     * If no items are contained, it says that "Your inventory is empty."
+     * @return A string representation of the container item and its contents.
+     */
     @Override
     public String toString() 
     {
